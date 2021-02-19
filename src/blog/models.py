84f2340ffc0f2efafdc893a350d5014d8f876fb0
aotantawy <ahmedosama9100@gmail.com>
2021-02-19 15:31:24 +0200

    add blog model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    author = models.ManyToManyField(User, blank=True, related_name='blogs')
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    views = models.IntegerField()
